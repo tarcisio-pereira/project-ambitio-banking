72c952fdcd1832d2ee3d26aa694f832e2af3ddd0
Tarcísio Pereira <tarcisio.95@hotmail.com>
2023-06-16 13:33:48 -0300

    setup database

---
+++ ambitio-banking/Data/AmbitioContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ambitio_banking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ambitio_banking.Data
+{
+	public class AmbitioContext : DbContext
+	{
+        protected readonly IConfiguration Configuration;
+
+        public AmbitioContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;port=3306;database=Banking;uid=root;password=senha123", new MySqlServerVersion(new Version(8, 0, 24)));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ ambitio-banking/Migrations/20230616155518_CriacaoTabelaUsuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ambitio_banking.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cpf = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Senha = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Datacadastro = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
